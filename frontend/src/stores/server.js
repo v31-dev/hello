2a3c3b3da66bdf1e4509c36f3c8ae2712a248bbd
sidxdev <sidsriv93@gmail.com>
2026-02-11 10:47:23 +0000

    socket sticky session support

--- frontend/src/stores/server.js
+++ frontend/src/stores/server.js
@@ -20,6 +20,7 @@ export const useServerStore = defineStore('server', () => {
       socket.value = io({ 
         path: '/api/ws/', 
         autoConnect: false,
+        withCredentials: true,
         extraHeaders: {
           Authorization: `Bearer ${token.value}`
         }
