9796fbb844d75b13c8b21e4799d4ea70d4459bef
sidxdev <sidsriv93@gmail.com>
2026-02-24 12:53:23 +0530

    offline_access scope

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -18,7 +18,7 @@ export const useAuthStore = defineStore('auth', () => {
     redirect_uri: `${window.location.origin}/callback`,
     post_logout_redirect_uri: `${window.location.origin}/`,
     response_type: 'code',
-    scope: 'openid profile',
+    scope: 'openid profile offline_access',
     automaticSilentRenew: true,
     userStore: new WebStorageStateStore({ store: window.localStorage }),
   })
